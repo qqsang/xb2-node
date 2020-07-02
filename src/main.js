4dc1813e9111224559fb0f5e768a51971e9b89bc
lishanchuang <992138278@qq.com>
2020-07-02 09:21:35 +0800

    创建路由

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.write('hello~');
+  const data = {
+    id: '1',
+    title: 'llldasd',
+    name: '李善创'
+  };
+  const jsondata = JSON.stringify(data);
+
+  switch (req.url) {
+    case '/':
+      res.write('hello~');
+      break;
+    case '/map':
+      res.write('map');
+      break;
+    case '/search':
+      res.write('search');
+      break;
+    case '/json':
+      res.writeHead(200, { 'Content-Type': 'application/json,charset=uft-8' });
+      res.write(jsondata);
+      break;
+    default:
+      res.writeHead(404);
+      res.write('404');
+      break;
+
+  }
   res.end();
 });
 server.listen(3000, () => { console.log('服务已启动'); });
