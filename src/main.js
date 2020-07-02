b261dd54b94ddff49a9e8c8a7d7e9a8586057517
lishanchuang <992138278@qq.com>
2020-07-02 13:20:07 +0800

    创建路由 给客户端响应内容

--- src/main.js
+++ src/main.js
@@ -1,19 +1,45 @@
-const http = require('http');
-const server = http.createServer((reg, res) => {
-  const data = {
-    name: 'lsc',
-    sex: 'man',
-    age: '18'
-  };
-  const jsondata = JSON.stringify(data);
-  switch (reg.url) {
-    case '/':
-      res.write('hello~');
-      break;
-    case '/json':
-      res.writeHead(200, { 'Conten-Type': 'application/json,charset=utf-8' });
-      res.write(jsondata);
-  }
-  res.end();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('服务已启动');
+})
+
+app.get('/', (req, res) => {
+  res.send('你好');
 });
-server.listen(3000, () => { console.log('服务已启动～') });
+
+const data = [
+  {
+    name: 'lsc',
+    age: 18,
+    id: 1
+  },
+  {
+    name: 'lxj',
+    age: 19,
+    id: 2
+  },
+  {
+    name: 'fal',
+    age: 20,
+    id: 3
+  }
+];
+app.get('/posts', (req, res) => {
+  res.send(data);
+});
+app.get('/posts/:postsId', (req, res) => {
+  //console.log(req.params);
+  //获取postsId
+  const { postsId } = req.params;
+
+  //查找具体内容
+  const post = data.filter(item => item.id == postsId);
+
+  //给客户端响应内容
+  res.send(post[0]);
+  //console.log(post);
+
+})
