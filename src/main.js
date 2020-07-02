f858fef32def1c250801134f8c212efc4b51caaa
lishanchuang <992138278@qq.com>
2020-07-02 10:00:43 +0800

    复习重写，创建服务器，处理json数据，switch做路由

--- src/main.js
+++ src/main.js
@@ -1,32 +1,19 @@
 const http = require('http');
-const server = http.createServer((req, res) => {
+const server = http.createServer((reg, res) => {
   const data = {
-    id: '1',
-    title: 'llldasd',
-    name: '李善创'
+    name: 'lsc',
+    sex: 'man',
+    age: '18'
   };
   const jsondata = JSON.stringify(data);
-
-  switch (req.url) {
+  switch (reg.url) {
     case '/':
       res.write('hello~');
       break;
-    case '/map':
-      res.write('map');
-      break;
-    case '/search':
-      res.write('search');
-      break;
     case '/json':
-      res.writeHead(200, { 'Content-Type': 'application/json,charset=uft-8' });
+      res.writeHead(200, { 'Conten-Type': 'application/json,charset=utf-8' });
       res.write(jsondata);
-      break;
-    default:
-      res.writeHead(404);
-      res.write('404');
-      break;
-
   }
   res.end();
 });
-server.listen(3000, () => { console.log('服务已启动'); });
+server.listen(3000, () => { console.log('服务已启动～') });
