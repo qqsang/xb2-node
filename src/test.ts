086a999c7991b0157e22bbd60d66f54f070d4193
lishanchuang <992138278@qq.com>
2020-07-03 12:12:11 +0800

    创建一个路由，并从express引入两个数据类型Request, Response

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,16 @@
 import express from "express";
+/**
+ * typescript提供了声明数据类型的功能，
+ * 而Request, Response这两个数据类型，是express这个框架声明的。
+ * 所以，要从express引入进来。这样理解对吗？
+ */
+import { Request, Response } from "express";
 const app = express();
 const port = 3000;
 app.listen(port, () => {
   console.log("test服务已启动");
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hi tester~");
 });
