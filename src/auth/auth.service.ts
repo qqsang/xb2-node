f022c448b21dc66cae1d80d1d9db2f84e5faaff2
lishanchuang <992138278@qq.com>
2020-07-13 17:31:26 +0800

    定义签发jwt令牌的服务

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { PRIVATE_KEY } from "../app/app.config";
+/**
+ * 签发令牌
+ */
+interface SignTokenOptions {
+  payload?: any;
+}
+
+export const signToken = (options: SignTokenOptions) => {
+  //准备选项
+  const { payload } = options;
+  //签发jwt
+  const token = jwt.sign(payload, PRIVATE_KEY, { algorithm: "RS256" });
+  //提供令牌
+  return token;
+};
