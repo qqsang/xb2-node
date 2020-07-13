5fab3efc48e89e8ff2a700e77ba70df4a681d7ec
lishanchuang <992138278@qq.com>
2020-07-13 11:04:44 +0800

    定义验证数据的中间件

--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -3,3 +3,20 @@
  */
 import { Request, Response, NextFunction } from "express";
 import * as userService from "./user.service";
+
+/**
+ * 定义一个中间件，验证注册用户的数据
+ */
+export const validataUserdata = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //从客户端提取用户注册时提交上来的数据
+  const { name, password } = req.body;
+  //验证必填数据
+  if (!name) return next(new Error("NAME_IS_REQUIRED"));
+  if (!password) return next(new Error("PASSWORD_IS_REQUIRED"));
+  //下一步
+  next();
+};
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import * as userController from "./user.controller";
+//导入验证用户的中间件，这个是自己定义好的。
+import { validataUserdata } from "../user/user.middleware";
 //创建路由
 const router = express.Router();
 //导出路由
@@ -8,5 +10,6 @@ export default router;
 /**
  * 创建接口
  * 给用户一个地址注册用户
+ * 使用中间件validataUserdata验证用户数据
  */
-router.post("/users", userController.store);
+router.post("/users", validataUserdata, userController.store);
