f2e95a351c66ae7b34f7dc22f2404331992edeee
lishanchuang <992138278@qq.com>
2020-07-13 14:12:27 +0800

    定义用户登录的接口并测试

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,2 +1,15 @@
-import express from "express";
+import { Request, Response, NextFunction } from "express";
 import * as userService from "../user/user.service";
+/**
+ * 用户登录处理器
+ */
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //活动用户登录的用户名和密码
+  const { name, password } = req.body;
+  //作出响应
+  res.send({ message: `欢迎回来，${name}` });
+};
--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,3 +1,8 @@
 import express from "express";
+import * as authController from "./auth.controller";
 const router = express.Router();
+/**
+ * 定义用户登录用的路由
+ */
+router.post("/login", authController.login);
 export default router;
