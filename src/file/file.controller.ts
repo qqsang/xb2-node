8c9eb35ecec9e60545118afc38ae2a3c7e245a1f
lishanchuang <992138278@qq.com>
2020-07-15 10:33:34 +0800

    定义上传文件的接口

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,1 +1,16 @@
 import { Request, Response, NextFunction } from "express";
+
+/**
+ * 上传文件
+ * @param req
+ * @param res
+ * @param next
+ */
+export const store = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log(req.file);
+  res.sendStatus(200);
+};
--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,4 +1,10 @@
 import express from "express";
 import * as fileController from "../file/file.controller";
+import { authGuard } from "../auth/auth.middleware";
+import { fileInterceptor } from "./file.middleware";
 const router = express.Router();
+/**
+ * 定义上传文件的接口
+ */
+router.post("/files", authGuard, fileInterceptor, fileController.store);
 export default router;
