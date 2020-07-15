38ab6380704483bac6b7a518138fdd9a4a0625df
lishanchuang <992138278@qq.com>
2020-07-15 15:47:54 +0800

    读取图像文件附加信息

--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from "express";
+//导入上传文件用的包multer
 import multer from "multer";
+//导入 读取和调整 图像文件的包jimp
+import Jimp from "jimp";
 
 /**
  * 创建一个multer
@@ -14,3 +17,26 @@ const fileUpload = multer({
  * single(‘file’)中‘file’是客户端上传文件用的表单字段起的名字
  */
 export const fileInterceptor = fileUpload.single("file");
+
+/**
+ * 处理文件使用的中间件
+ * 文件处理器
+ */
+export const fileProcessor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //获取客户端请求的要处理的文件路径,需要放在上传文件中间件后面才能获得上传文件请求中的path
+  const { path } = req.file;
+  let image: Jimp;
+  try {
+    //读取图像文件
+    image = await Jimp.read(path);
+    console.log(image);
+  } catch (error) {
+    return next(error);
+  }
+  //下一步
+  next();
+};
--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import * as fileController from "../file/file.controller";
 import { authGuard } from "../auth/auth.middleware";
-import { fileInterceptor } from "./file.middleware";
+import { fileInterceptor, fileProcessor } from "./file.middleware";
 const router = express.Router();
 /**
  * 定义上传文件的接口
  */
-router.post("/files", authGuard, fileInterceptor, fileController.store);
+router.post(
+  "/files",
+  authGuard,
+  fileInterceptor,
+  fileProcessor,
+  fileController.store
+);
 /**
  * 定义带地址参数请求查找文件的路由
  */
