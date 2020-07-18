2ed9295ddf15fd4db71c095065925e15aa6f1eed
lishanchuang <992138278@qq.com>
2020-07-18 11:51:44 +0800

    定义按标签名查找标签的功能

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -4,7 +4,7 @@ import { tagModel } from "./tag.model";
 /**
  * 创建标签
  */
-const createTag = async (tag: tagModel) => {
+export const createTag = async (tag: tagModel) => {
   //准备查询
   const statement = `
   INSERT INTO tag
@@ -15,4 +15,20 @@ const createTag = async (tag: tagModel) => {
 
   //返回数据
   return data as any; //返回任意类型的data数据
+};
+
+/**
+ * 定义按标签名字查找标签
+ */
+export const getTagByName = async (tagName: string) => {
+  //准备查询
+  const statement = `
+  SELECT id,name FROM tag
+  WHERE name = ?`;
+
+  //执行查询
+  const [data] = await connection.promise().query(statement, tagName);
+
+  //提供数据
+  return data[0];
 };
