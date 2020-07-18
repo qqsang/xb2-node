eef6fc53feb8a38a358553a19584e96bebc6d1d5
lishanchuang <992138278@qq.com>
2020-07-18 12:05:25 +0800

    定义创建标签用的接口

--- src/tag/tag.router.ts
+++ src/tag/tag.router.ts
@@ -1,4 +1,9 @@
 import express from "express";
 import * as tagController from "./tag.controller";
+import { authGuard } from "../auth/auth.middleware";
 const router = express.Router();
+/**
+ * 定义创建标签的路由
+ */
+router.post("/tags", authGuard, tagController.store);
 export default router;
