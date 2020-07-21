b14e40ea0e19c6db678cbe857c98e6aec86c5dcb
lishanchuang <992138278@qq.com>
2020-07-21 14:49:04 +0800

    定义保存内容标签的服务

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -86,3 +86,19 @@ export const deletePosts = async (postId: number) => {
   const [data] = await connection.promise().query(statement, postId);
   return data;
 };
+
+/**
+ * 创建内容tag
+ */
+export const creatPostTag = async (postId: number, tagId: number) => {
+  //准备查询
+  const statement = `
+    INSERT INTO post_tag (postId,tagId)
+    VALUES(?,?)`;
+
+  //执行查询
+  const [data] = await connection.promise().query(statement, [postId, tagId]);
+
+  //提供数据
+  return data;
+};
