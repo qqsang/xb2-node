d948612986c8cad5e45f676523e2e85283c68666
lishanchuang <992138278@qq.com>
2020-07-22 14:47:08 +0800

    定义检查评论是否为回复评论的功能

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -17,3 +17,19 @@ export const createComment = async (comment: CommentModel) => {
   //返回结构
   return data as any;
 };
+
+/**
+ * 定义检查评论是否为回复评论的服务
+ */
+export const isReplayComment = async (commentId: number) => {
+  //准备查询
+  const statement = `
+  SELECT parentId FROM comment
+  WHERE id = ?`;
+
+  //执行查询
+  const [data] = await connection.promise().query(statement, commentId);
+
+  //提供结果
+  return data[0].parentId ? true : false;
+};
