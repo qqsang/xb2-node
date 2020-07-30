eac67faadc31ce46cfa5f71c0348ff25f3985489
lishanchuang <992138278@qq.com>
2020-07-30 21:18:26 +0800

    定义按用户id查找头像

--- src/avatar/avatar.service.ts
+++ src/avatar/avatar.service.ts
@@ -15,3 +15,23 @@ export const createAvatar = async (avatar: AvatarModel) => {
   //返回结果
   return data;
 };
+
+/**
+ * 定义按用户id查找头像功能
+ */
+export const findAvatarByUserId = async (userId: number) => {
+  //准备查询
+  const statement = `
+  SELECT *
+  FROM avatar
+  WHERE userId = ?
+  ORDER BY avatar.id DESC
+  LIMIT 1
+  `;
+
+  //执行查询
+  const [data] = await connection.promise().query(statement, userId);
+
+  //返回结果
+  return data[0];
+};
