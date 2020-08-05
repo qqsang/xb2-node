25324c1a7967270c0c869a9ad3b0799f1989d6a1
lishanchuang <992138278@qq.com>
2020-08-05 12:02:03 +0800

    定义取消用户点赞的服务

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -17,3 +17,21 @@ export const createUserLikePost = async (userId: number, postId: number) => {
   //提供数据
   return data;
 };
+
+/**
+ * 定义取消用户点赞内容的服务
+ */
+export const deleteUserLikePost = async (userId: number, postId: number) => {
+  //准备sql语句
+  const statement = `
+    DELETE FROM
+      user_like_post
+    WHERE userId = ? AND postId = ?
+  `;
+
+  //执行查询
+  const [data] = await connection.promise().query(statement, [userId, postId]);
+
+  //提供数据
+  return data;
+};
