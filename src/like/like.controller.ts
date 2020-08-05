2433c6c6308778beaa08168d6c4fa8436728ab8a
lishanchuang <992138278@qq.com>
2020-08-05 11:56:02 +0800

    定义处理点赞内容的接口处理器

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,1 +1,25 @@
 import { Request, Response, NextFunction } from "express";
+import { createUserLikePost } from "./like.service";
+
+/**
+ * 定义用户点赞内容接口处理器
+ */
+export const storeUserLikePost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //准备数据
+  const { postId } = req.params;
+  const { id: userId } = req.user;
+
+  try {
+    //用户点赞
+    const like = await createUserLikePost(userId, parseInt(postId, 10));
+
+    //作出响应
+    res.status(201).send(like);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/like/like.router.ts
+++ src/like/like.router.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import * as likeController from "./like.controller";
+import { authGuard } from "../auth/auth.middleware";
 const router = express.Router();
-
+router.post("/posts/:postId/like", authGuard, likeController.storeUserLikePost);
 /**
  * 导出路由
  */
