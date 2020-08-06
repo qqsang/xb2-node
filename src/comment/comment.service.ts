e767d9fc6b8292f6b93f3166ec9bf2d32cab2cb9
lishanchuang <992138278@qq.com>
2020-08-06 13:30:59 +0800

    定义获取评论列表的功能

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,5 +1,6 @@
 import { connection } from "../app/database/mysql";
 import { CommentModel } from "./comment.model";
+import { sqlFragment } from "./comment.provider";
 
 /**
  * 定义存储评论数据的服务
@@ -65,6 +66,37 @@ export const deleteComment = async (commentId: number) => {
 
   //执行删除操作
   const [data] = await connection.promise().query(statement, commentId);
+
+  //提供结果
+  return data;
+};
+
+/**
+ * 定义获取评论列表的服务
+ */
+export const getComments = async () => {
+  //定义参数
+  let params: Array<any> = [];
+
+  //准备查询
+  const statement = `
+    SELECT 
+      comment.id,
+      comment.content,
+      ${sqlFragment.user},
+      ${sqlFragment.post}
+    FROM
+      comment
+    ${sqlFragment.leftjoinuser}
+    ${sqlFragment.leftjoinpost}
+    GROUP BY 
+      comment.id
+    ORDER BY
+      comment.id DESC
+  `;
+
+  //执行查询
+  const [data] = await connection.promise().query(statement, params);
 
   //提供结果
   return data;
