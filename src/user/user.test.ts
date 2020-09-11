64a4bd69e97b1b1663f378f4ae4d28d7b00b20dc
lishanchuang <992138278@qq.com>
2020-09-11 13:33:31 +0800

    自动测试-测试更新用户

--- src/user/user.test.ts
+++ src/user/user.test.ts
@@ -97,7 +97,7 @@ describe("测试创建用户接口", () => {
 /**
  * 测试用户账户
  */
-describe("测试用户账户接口", async () => {
+describe("测试用户账户接口", () => {
   test("响应里应该包含指定属性", async () => {
     //发出请求
     const response = await request(app).get(`/user/${testUserCreated.id}`);
@@ -116,5 +116,49 @@ describe("测试用户账户接口", async () => {
     const response = await request(app).get("/users/-1");
     //作出断言
     expect(response.status).toBe(404);
+  });
+});
+
+/**
+ * 测试更新用户
+ */
+describe("测试更新用户", () => {
+  test("更新用户需要验证身份", async () => {
+    const response = await request(app).patch("/users");
+    //作出断言
+    expect(response.status).toBe(401);
+  });
+
+  test("更新用户数据", async () => {
+    //签发令牌
+    const token = signToken({
+      payload: { id: testUserCreated.id, name: testUserCreated.name },
+    });
+    //请求接口
+    const response = await request(app)
+      .patch("/users")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        validate: {
+          password: testUser.password,
+        },
+        update: {
+          name: testUpdateUser.name,
+          password: testUpdateUser.password,
+        },
+      });
+    //获取用户
+    const user = await getUserById(testUserCreated.id, { password: true });
+
+    //对比密码
+    const matched = await bcrypt.compare(
+      testUpdateUser.password,
+      user.password
+    );
+
+    //作出断言
+    expect(response.status).toBe(200);
+    expect(matched).toBeTruthy();
+    expect(user.name).toBe(testUpdateUser.name);
   });
 });
